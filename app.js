a00b521111b4a1ea11f45e361940819e80dcd820
Nathan Cosgrove <ncosgrove272@gmail.com>
2023-09-24 00:32:05 -0500

    Created "If" Conditional statement

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+// concatenated string variable below // 
+
 const food = "chocalate";
 const drink = "milk"; 
 const food2 = "waffles";
 const amount = 2; 
 
-console.log(`One of my favorite breafest combinations is ${food} ${drink} with ${amount} ${food2} and syrup.`)
+console.log(`One of my favorite breafest combinations is ${food} ${drink} with ${amount} ${food2} and syrup.`);
 
-// concatenated string variable above // 
+// "If" Conditional statement below // 
 
+const greeting = 11;
 
+if (greeting <= 11 ) {
+  console.log("Rise and shine!");
+} else if (greeting === 12) {
+  console.log("Did you enjoy sleeping in?");
+} else if (greeting > 12) {
+  console.log("Time to relax and unwind.");
+} else {
+  console.log("Hello there.");
+}
